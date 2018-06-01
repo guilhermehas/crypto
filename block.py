2b79552f2913f999f7c7fe3b6134a7c68c05e94f
Guilherme <guilhermehas@hotmail.com>
2018-06-01 01:48:53 -0300

    Add tests to verify that different values have different hashes

--- block.py
+++ block.py
@@ -1,6 +1,10 @@
 class Block:
-    def __init__(self, data):
+    def __init__(self, previous_block, data: str):
         self.data = data
+        self.previous_hash = hash(previous_block)
+    
+    def __hash__(self):
+        return hash((self.previous_hash, self.data))
 
 def GenesisBlock(data: str):
-    return Block(data=data)
+    return Block(previous_block=None, data=data)
--- test_block.py
+++ test_block.py
@@ -1,6 +1,29 @@
+import pytest
 from block import *
 
-def test_first_block():
-    data = 'abc'
+@pytest.mark.parametrize("data", 
+    ["abc", "cdb", "234", "44444"])
+def test_first_block(data: str):
     genesis_block = GenesisBlock(data=data)
     assert genesis_block.data == data
+
+@pytest.fixture
+def genesis_block():
+    return GenesisBlock(data="abc")
+
+@pytest.mark.parametrize("data", 
+    ["abc", "cdb", "234", "44444"])
+def test_second_block(data):
+    block = Block(previous_block=genesis_block, data=data)
+    assert block.data == data
+    assert hash(genesis_block) == block.previous_hash
+
+@pytest.mark.parametrize("data1,data2", [ 
+    (None, ''),
+    ('', '0'),
+    ('abc', 'ab'),
+    ('1', 1),
+ ])
+def test_diferent_hashes(data1, data2):
+    block1, block2 = map(lambda data: GenesisBlock(data), (data1,data2))
+    assert hash(block1) != hash(block2)
