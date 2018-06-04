ec8e295ad7a961bdb6f473e7c2d354311cbe91ab
Guilherme <guilhermehas@hotmail.com>
2018-06-04 18:35:44 -0300

    update serverUtils

--- server.py
+++ server.py
@@ -2,13 +2,27 @@ from pickle import dumps, loads
 import pickle
 from base64 import b64encode, b64decode
 from blockchain import *
+from transaction_pool import *
+from block import *
 
 class ServerUtils():
-    def __init__(self):
+    def __init__(self, wallet):
         self.blockchain =  Blockchain()
+        self.transactionPool = TransactionPool()
+        self.wallet = wallet
     
     def receive_block_array(self, blockArray):
         self.blockchain.substitute(blockArray)
+    
+    def receive_transaction(self, transaction):
+        self.transactionPool.receive_transaction(self.blockchain, transaction)
+    
+    def mine(self):
+        transactions = self.transactionPool.get_best_transactions(self.blockchain, 1)
+        block = Block(self.blockchain.get_last_block(), transactions,  \
+            miner_pub_key=self.wallet.get_public_key_in_bytes())
+        block.mine(self.blockchain.get_difficult())
+        self.blockchain.add(block)
     
     def to_str(self, obj):
         return str(b64encode(dumps(obj, protocol=pickle.HIGHEST_PROTOCOL)), 'utf8')
--- test_server.py
+++ test_server.py
@@ -3,20 +3,34 @@ import pytest
 from examples_to_test import *
 from server import *
 from examples_to_test import blockchain as get_blockchain
+from wallet import *
 
 @pytest.fixture
 def blockchain():
     return get_blockchain()
 
-def test_same_transaction():
+@pytest.fixture
+def server():
+    wallet= Wallet(3)
+    return ServerUtils(wallet)
+
+def test_same_transaction(server):
     transaction = transactions[0]
-    server = ServerUtils()
     transaction_str = server.to_str(transaction)
     transaction2 = server.to_obj(transaction_str)
     assert transaction.is_equal(transaction2)
 
-def test_receive_blockArray(blockchain):
-    server = ServerUtils()
+def test_receive_blockArray(blockchain, server):
     blockArray = BlockArray(blockchain)
     server.receive_block_array(blockArray)
     assert len(server.blockchain) == 3
+
+def test_receive_transaction(server, blockchain):
+    transaction = transactions[0]
+    len_blockchain_before = len(server.blockchain)
+
+    server.receive_transaction(transaction)
+    server.mine()
+
+    len_blockchain_after = len(server.blockchain)
+    assert len_blockchain_after == len_blockchain_before + 1
