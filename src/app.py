6419aeaabab39bb3b234b908aef40e7dacd80ea5
Guilherme <guilhermehas@hotmail.com>
2018-06-13 00:04:24 -0300

    Add receive_blockchain

--- src/app.py
+++ src/app.py
@@ -1,16 +1,23 @@
 from json import loads
+from base64 import b64decode
+import pickle
 
 from flask import Flask, jsonify, request
 from forms import TransactionForm
 
-from blockchain import Blockchain
-from block import GenesisBlock
+from blockchain import Blockchain, BlockArray
+from transaction_pool import TransactionPool
+from transaction import Transaction
+from wallet import Wallet
+from block import GenesisBlock, Block
 from crypto import PublicKey, PrivateKey
 
 def create_app():
     app = Flask(__name__)
     blockchain = Blockchain()
-    miner_key = PrivateKey(1).public_key().to_bytes()
+    transactionPool = TransactionPool()
+    miner_key_int = 1
+    miner_key = PrivateKey(miner_key_int).public_key().to_bytes()
 
     @app.route('/blockchain')
     def blockchain_web():
@@ -18,16 +25,38 @@ def create_app():
 
     @app.route('/addtransaction', methods=['POST'])
     def add_transaction():
-        form = TransactionForm(**request.json)
+        transaction_dict = request.json
+        form = TransactionForm(**transaction_dict)
         if form.validate():
+            wallet = Wallet(miner_key_int)
+            transaction = Transaction(outputs=[ 
+                (PrivateKey(transaction_dict['receiver']).public_key().to_bytes(),
+                transaction_dict['amount'])])
+            signed_transaction = wallet.sign(transaction)
+            transactionPool.receive_transaction(blockchain, signed_transaction)
             return ''
         return '', 403
     
     @app.route('/mine')
     def mine():
-        genesis_block = GenesisBlock(miner_pub_key=miner_key)
-        genesis_block.mine(blockchain.get_difficult())
-        blockchain.add(genesis_block)
+        if len(blockchain) == 0:
+            genesis_block = GenesisBlock(miner_pub_key=miner_key)
+            genesis_block.mine(blockchain.get_difficult())
+            blockchain.add(genesis_block)
+        else:
+            block = Block(previous_block=blockchain.chain[-1], miner_pub_key=miner_key, \
+                transactions=transactionPool.get_best_transactions(blockchain,1))
+            block.mine(blockchain.get_difficult())
+            blockchain.add(block)
+
+        return ''
+    
+    @app.route('/receiveblockchain', methods=['POST'])
+    def receive_blockchain():
+        blockArray = pickle.loads(b64decode(request.data))
+        if isinstance(blockArray, BlockArray):
+            blockchain.substitute(blockArray)
+            return ''
         return ''
 
     return app
--- test/test_app.py
+++ test/test_app.py
@@ -2,16 +2,22 @@
 import pytest
 from flask import url_for
 from json import dumps
+from base64 import b64encode
+import pickle
+
+from examples_to_test import blockchain
+from blockchain import BlockArray
 
 MINER_PUB_KEY = 1
 MINER_REWARD = 10
+AMOUNT_TRANS = 1
 
 @pytest.fixture
 def transaction():
     transaction_dict = {
         'sender': MINER_PUB_KEY,
         'receiver': 2,
-        'amount': 1
+        'amount': AMOUNT_TRANS
     }
     return transaction_dict
 
@@ -20,14 +26,15 @@ def test_get_blockchain(client):
     assert res.status_code == 200
     assert res.json == {'balances': {}, 'blocks': []}
 
-def add(transaction):
-    class ClassNameLess:
-        def to(self, client):
+class add:
+    def __init__(self, transaction):
+        self.transaction = transaction
+    def to(self,client):
             res = client.post(url_for('add_transaction'), \
-                            data=dumps(transaction), \
+                            data=dumps(self.transaction), \
                             content_type='application/json')
             return res
-    return ClassNameLess()
+
 
 def test_add_transaction(client, transaction):
     res = add(transaction).to(client)
@@ -44,4 +51,37 @@ def test_mining(client):
     
     res = client.get('blockchain')
     blockchain = res.json
-    assert blockchain['balances'][str(MINER_PUB_KEY)] == MINER_REWARD
+    balances = blockchain['balances']
+    assert balances[str(MINER_PUB_KEY)] == MINER_REWARD
+
+def test_two_mining(client):
+    client.get('mine')
+    client.get('mine')
+    
+    res = client.get('blockchain')
+    blockchain = res.json
+    balances = blockchain['balances']
+    assert balances[str(MINER_PUB_KEY)] == 2*MINER_REWARD
+
+def test_mining_with_transactions(client, transaction):
+    client.get('mine')
+    add(transaction).to(client)
+    client.get('mine')
+    
+    res = client.get('blockchain')
+    blockchain = res.json
+    balances = blockchain['balances']
+    assert balances[str(MINER_PUB_KEY)] == 2*MINER_REWARD-AMOUNT_TRANS
+
+def test_receive_blockchain(client, blockchain):
+    blockArray = BlockArray(blockchain)
+    blockchain_bytes = b64encode(pickle.dumps(blockArray))
+    res = client.post(url_for('receive_blockchain'), \
+                      data=blockchain_bytes)
+    
+    assert res.status_code == 200
+
+    res = client.get('blockchain')
+    blockchain = res.json
+    assert len(blockchain['blocks']) == 3
+    
