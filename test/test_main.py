5415fb5bd64a2e714556c292bdb19d05957971e9
Guilherme <guilhermehas@hotmail.com>
2018-06-15 01:02:13 -0300

    Add test get server

--- test/test_main.py
+++ test/test_main.py
@@ -1,7 +1,7 @@
 import pytest
 import sys
 
-from main import get_args
+from main import get_args, get_server
 
 def test_get_args():
     sys.argv = ['','--port', '8000']
@@ -10,3 +10,8 @@ def test_get_args():
     assert args.servers == []
     assert isinstance(args.wallet, int)
     assert args.wallet > 0
+
+def test_get_server():
+    args = get_args()
+    server = get_server(args)
+    assert server is not None
